3f6c90ad53e68ef0e14a5dd5ac28a5cf91508e5a
Gabriel Reus <Gabriel.Reus@gmail.com>
2024-08-10 21:48:17 +0200

    More tests

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 22:46:31 by gabriel           #+#    #+#             */
-/*   Updated: 2024/08/10 21:45:05 by gabriel          ###   ########.fr       */
+/*   Updated: 2024/08/10 21:47:55 by gabriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -99,6 +99,28 @@ int main()
 		me->use(67, *bob);
 		me->use(-1, *bob);
 		me->use(3, *bob);
+		delete bob;
+		delete me;
+		delete src;		
+	}
+	std::cout << "Test unequip ******************* " << std::endl;
+	{
+		IMateriaSource* src = new MateriaSource();
+		src->learnMateria(new Ice());
+		src->learnMateria(new Cure());
+		ICharacter* me = new Character("me");
+		AMateria* tmp;
+		me->unequip(1);
+		tmp = src->createMateria("ice");
+		me->equip(tmp);
+		tmp = src->createMateria("cure");
+		me->equip(tmp);
+		ICharacter* bob = new Character("bob");
+		me->use(0, *bob);
+		me->use(1, *bob);
+		std::cout <<"\tWe are going to unequip 1." << std::endl;
+		me->unequip(1);
+		me->use(1, *bob);
 		delete bob;
 		delete me;
 		delete src;		
